c57d7d06d9078853a17c5607a8718b150caca55d
SeolA-Lee <dlcodus1201@naver.com>
2023-11-17 23:30:51 +0900

    today third commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   };
 
   return(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="myPortfolio">
         <header className="myHeader">
           {/* Header Title */}
